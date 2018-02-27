9ae8d79b3826e7cdcb575981568e04ef7f7a7a91
Eloe,Wendy C <s524779@ch140029m.nwmissouri.edu>
2018-02-27 16:02:48 -0600

    Initial Commit

---
+++ SecretCode_Eloe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecretCode_Eloe
+//
+//  Created by Eloe,Wendy C on 2/27/18.
+//  Copyright © 2018 Eloe,Wendy C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
